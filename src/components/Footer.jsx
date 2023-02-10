96ea66b0fc2aa9530e1869878211551519d8c6b7
Tyler Kotaka <tkotaka45@gmail.com>
2023-02-10 14:29:56 -0500

    Adjusted footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import * as styles from './styles/footer.module.css'
-
+import gatsImg from '../../static/gatsbyIcon.png'
 
 function Footer() {
     return (
-        <div className ={styles.container}>
-            <a id={styles.contact}href='/contact'>Contact Me</a>
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <a id={styles.contact} href='/contact'>Contact Me</a>
+
+                <div id={styles.nameSake}>
+                    {' '}
+                    Tyler Kotaka {(new Date().getFullYear())}
+                    {' '}
+                    <img className={styles.icons} src={gatsImg}></img>
+                </div>
+                <div id={styles.deploy}>
+                    Deployed on AWS Amplify
+                </div>
+            </div>
         </div>
     )
 }
