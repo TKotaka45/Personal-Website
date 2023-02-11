f6fbdcfbc7d34419b34a8f9c2cb3bc6e0046f90d
Tyler Kotaka <tkotaka45@gmail.com>
2023-02-11 17:29:21 -0500

    Added check for document when rendering page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import * as styles from '../components/styles/contact.module.css'
@@ -6,14 +7,14 @@ import * as styles from '../components/styles/contact.module.css'
 
 function Contact() {
     return (
-        <div style={{minHeight:'100vh', display:'flex',  flexDirection:'column', overflow:'hidden'}}>
+        <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
 
             <Header />
 
             <main id={styles.contactForm}>
                 Contact Me:
                 <div id={styles.uInput}>
-                    <div style={{ gridArea: 'n'}}>
+                    <div style={{ gridArea: 'n' }}>
                         <div>
                             <label for='name'>Name: </label>
                             <input type='text' name='name' id='name' placeholder='Name' />
@@ -21,7 +22,7 @@ function Contact() {
 
                     </div>
 
-                    <div style={{ gridArea: 'e'}}>
+                    <div style={{ gridArea: 'e' }}>
                         <div>
                             <label for='email'>Email: </label>
                             <input type='text' name='email' id='email' placeholder='Email Address'></input>
@@ -29,7 +30,7 @@ function Contact() {
 
                     </div>
 
-                    <div style={{ gridArea: 'm'}}>
+                    <div style={{ gridArea: 'm' }}>
                         <div>
                             <label for='message'>Message: </label>
                             <textarea name='message' id='message' placeholder='Message'></textarea>
@@ -37,7 +38,7 @@ function Contact() {
 
                     </div>
 
-                    <div style={{ gridArea: 'l'}}>
+                    <div style={{ gridArea: 'l' }}>
                         <div>
                             <label for='location'> Where are you from? </label>
                             <input type='text' id='location' placeholder='City, State'></input>
@@ -45,7 +46,7 @@ function Contact() {
 
                     </div>
 
-                    <div style={{ gridArea: 's', postition:'relative'}}>
+                    <div style={{ gridArea: 's', postition: 'relative' }}>
                         <input type='submit' value='Submit'></input>
                     </div>
                 </div>
@@ -58,10 +59,12 @@ function Contact() {
 
 export default Contact
 
-const tx = document.getElementsByTagName("textarea");
-for (let i = 0; i < tx.length; i++) {
-    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-    tx[i].addEventListener("input", OnInput, false);
+if (typeof document !== "undefined") {
+    const txtarea = document.getElementsByTagName("textarea");
+    for (let i = 0; i < txtarea.length; i++) {
+        txtarea[i].setAttribute("style", "height:" + (txtarea[i].scrollHeight) + "px;overflow-y:hidden;");
+        txtarea[i].addEventListener("input", OnInput, false);
+    }
 }
 
 function OnInput() {
