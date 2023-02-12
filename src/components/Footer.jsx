9c89bf86c16668f24577fa744ddc5873874bf9ef
Tyler Kotaka <tkotaka45@gmail.com>
2023-02-12 10:48:54 -0500

    Added footer funcationality, added resume download

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,16 @@ function Footer() {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <a id={styles.contact} href='/contact'>Contact Me</a>
+
+                <a href='/' style={{gridArea:'3/2/4/3'}}>Home</a>
+                <a href='/projects' style={{gridArea:'5/2/6/3'}}>Projects</a>
+                <a href='/resume' style={{gridArea:'7/2/8/3'}}>Resume</a>
+
+                <a href='/contact' style={{gridArea:'3/4/4/5'}}>Contact Me</a>
+                <a href='https://www.linkedin.com/in/tyler-kotaka' style={{gridArea:'5/4/6/5'}} target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href='https://github.com/TKotaka45' style={{gridArea:'7/4/8/5'}} target="_blank" rel="noreferrer">GitHub</a>
+
+
 
                 <div id={styles.nameSake}>
                     {' '}
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import resumeImg from '/static/resumeImg.png'
+import TylerKotakaResume from '../../static/resume.pdf'
 import * as resume from '../components/styles/resume.module.css'
 
 
@@ -11,7 +12,12 @@ function Resume() {
     return (
         <>
             <Header />
-            <img id={resume.image} src={resumeImg} alt="An Image of my resume, click to open PDF in new window"></img>
+            <div style={{width:'60rem', margin:'10rem auto 0 auto'}}>
+                <img id={resume.image} src={resumeImg} alt="An Image of my resume, click to open PDF in new window"></img>
+                <a href={TylerKotakaResume} download='TylerKotakaResume.pdf' style={{fontSize:'1.5rem', float:'right', color:'#cccccc', fontWeight:'200'}}>Download Resume PDF</a>
+            </div>
+            
+
             <Footer />
 
         </>
